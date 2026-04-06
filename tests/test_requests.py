393d52b5dc8cb559cd054fa784f21bef0a9c02fb
Matthew-Ingram <99311642+Matthew-Ingram@users.noreply.github.com>
2026-04-06 12:32:37 -0500

    Updated Python files for database functionality. (#74)

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -7,26 +7,26 @@ def test_get_connection_without_error():
     connection.close()
 
 def test_view_requests_witout_error():
-    view_requests("tempemail@test.com", "temppassword123")
+    view_requests("avery.wright@test.local", "password7")
 
 def test_create_normal_request_without_error():
-    create_request("test_title", "test_description", "Low", "tempemail@test.com", "temppassword123")
+    create_request("test_title", "test_description", 1, "avery.wright@test.local", "password7")
 
 def test_normal_update_status_without_error():
-    assert update_status(1, "Closed", "tempemail@test.com", "temppassword123") == True
+    assert update_status(1010, 2, "avery.wright@test.local", "password7") == True
 
 def test_create_abnormal_request_with_error():
     with pytest.raises(sqlite3.Error):
-        create_request("test_title2", "test_description2", "Banana", "tempemail@test.com", "temppassword123")
+        create_request("test_title2", "test_description2", "Banana", "avery.wright@test.local", "password7")
 
 def test_abnormal_update_status_with_error():
-    assert update_status(-5, "Something", "tempemail@test.com", "temppassword123") == False
+    assert update_status(-5, 7, "avery.wright@test.local", "password7") == False
 
 def test_sort_by_priority_without_error():
-    sort_by_priority("tempemail@test.com", "temppassword123")
+    sort_by_priority("avery.wright@test.local", "password7")
 
 def test_normal_filter_by_status_without_error():
-    filter_by_status("Open", "tempemail@test.com", "temppassword123")
+    filter_by_status(0, "avery.wright@test.local", "password7")
 
 def test_abnormal_filter_by_status_with_error():
-    assert filter_by_status("Status", "tempemail@test.com", "temppassword123") == 0
+    assert filter_by_status(-2, "avery.wright@test.local", "password7") == 0
